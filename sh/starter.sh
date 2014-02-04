82872e2e31b53f1ee291d0afb80031dcf68ee416
f13mash <mashf13@gmail.com>
2014-02-04 05:30:42 +0530

    +x perm

--- sh/starter.sh
+++ sh/starter.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-
-if [ $(ps -e -o uid,cmd | grep $UID | grep node  | wc -l | tr -s "\n") -eq 0 ]
-then
-        export PATH=/usr/local/bin:$PATH
-        cd /root/projects/maes-io
-        forever start --sourceDir /usr/bin/node app.js >> logs/log.txt 2>&1
-fi
+cd /root/projects/maes-io
+forever start --sourceDir /usr/bin/node app.js >> logs/log.txt 2>&1
